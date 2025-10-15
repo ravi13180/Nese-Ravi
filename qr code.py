201bc2eda0ca201fa9b1209dd9111c0423f22aea
Nese Ravi <24B11DS149@adityauniversity.in>
2025-10-15 13:56:10 +0530

    Add files via upload

---
+++ qr code.py
@@ -0,0 +1,20 @@
+"""import  qrcode
+data="https://www.hackerrank.com/profile/suryanarayanamu3"
+qr=qrcode.make(data)
+qr.save("hackerrank.png")
+print("QR code generated and save as 'hackerrank.png'")
+
+import qrcode
+from PIL import Image
+data="https://www.instagram.com/life___time___settlement___66/"
+qr=qrcode.make(data)
+qr.save("instagram.png")
+print("QR code generated and save as 'instagram.png'")
+numbers=[10,20,30,40,50]
+largest=max(numbers)
+print("the largest number is",largest)"""
+import qrcode
+data="https://www.youtube.com/@Prasadtechintelugu"
+qr=qrcode.make(data)
+qr.save("youtube.png")
+print("qr codwe generated and save as youtube.png")
